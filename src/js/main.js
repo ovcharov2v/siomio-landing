11f931e8e3940d5347f94912a4f058b0ca8202d0
Viktor Ovcharov <ovcharov.2v@gmail.com>
2023-08-31 23:05:41 +0400

    feat: add new animation

--- src/js/main.js
+++ src/js/main.js
@@ -3,42 +3,51 @@ import './vendor';
 import './components/_mobile-menu';
 
 import gsap from 'gsap'
-import {ScrollTrigger} from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
+import {ScrollTrigger} from "gsap/ScrollTrigger"
+gsap.registerPlugin(ScrollTrigger)
 
 const mm = gsap.matchMedia()
 
 mm.add('(min-width: 1024px)', () => {
-	ScrollTrigger.create({
-		trigger: ".main",
-		start: "1px top",
-		toggleActions: "restart none none reverse",
-		snap: {
-			snapTo: 1 / 3,
-			duration: .5,
-			delay: 0.0,
-			ease: "none",
-		}
-	})
-
 	const sections = document.querySelectorAll('.section--hero, .section--swipe')
 
 	sections.forEach((section, index) => {
 		gsap.timeline().to(section,  {
-			y: () => window.innerHeight,
 			scale: .8,
 			rotate: 15,
-			opacity: 0,
-			duration:.5,
 			ease: "linear",
+			y: 300,
 			scrollTrigger: {
 				trigger: section,
+				pinSpacing: false,
+				pin: true,
+				pinnedContainer: ".main",
 				start: '1% top',
-				toggleClass: "active",
+				end: "220% center",
 				scrub:1,
+				anticipatePin: 1,
 				toggleActions: "restart none none reset",
 				//markers: true,
 			}
 		})
 	})
+	sections.forEach((section, index) => {
+		gsap.timeline().to(section,  {
+			ease: "linear",
+			scrollTrigger: {
+				trigger: section,
+				pinnedContainer: ".main",
+				start: '1% top',
+				//markers: true,
+				onLeave: () => {
+					section.classList.add('v-hide')
+				},
+				onEnterBack: () => {
+					section.classList.remove('v-hide')
+				},
+			}
+		})
+	})
 })
+
+
